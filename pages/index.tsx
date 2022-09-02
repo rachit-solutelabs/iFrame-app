7a60340e87eb3d36ed086e5f355f94485060a8db
Rachit Thakker <rachit.t@solutelabs.com>
2022-09-02 18:05:39 +0530

    Added metadata to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import HomeLayout from "../src/components/HomeLayout";
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <div className="intro-gradient" />
-      <HomeLayout />
-    </main>
+    <>
+      <Head>
+        <title>iFrame App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Render webpages inside an iframe" />
+      </Head>
+      <main>
+        <div className="intro-gradient" />
+        <HomeLayout />
+      </main>
+    </>
   );
 };
 
